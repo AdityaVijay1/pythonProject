6452af3a9b8ee0b22c79dab8b008bf664905a0a2
AdityaVijay1 <adityavijay1712@gmail.com>
2022-11-14 16:20:16 +0800

    Refactor result based on score with Pseudocode

--- CP1404 Practicals/prac_02/score.py
+++ CP1404 Practicals/prac_02/score.py
@@ -1,33 +1,60 @@
 """
-CP1404 - Practical Week 1 broken_score.py
+CP1404 - Practical Week 2 score.py
 Broken program to determine score status
 
 Pseudocode
 
-get score
-while score < 0 or score > 100:
-    display "Invalid score"
+function main():
+    score = get_score()
+    result = get_result(score)
+    display result
+
+
+function get_score():
     get score
-else:
+
+    while score < 0 or score > 100:
+        display "Invalid score"
+        get score
+    return score
+
+
+function get_result(score):
     if score >= 90:
-        display "Excellent"
+        return "Excellent"
     elif score >= 50:
-        display "Passable"
+        return "Passable"
     else:
-        display "Bad"
+        return "Bad"
+
+
+main()
 
 """
 
-score = float(input("Enter score: "))
 
-while score < 0 or score > 100:
-    print("Invalid score")
+def main():
+    score = get_score()
+    result = get_result(score)
+    print(result)
+
+
+def get_score():
     score = float(input("Enter score: "))
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    else:
-        print("Bad")
 
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+
+def get_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
