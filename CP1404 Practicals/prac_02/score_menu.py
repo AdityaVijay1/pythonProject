ee99e008d95ca9db17cdd7ef7820189e73b6e72f
AdityaVijay1 <adityavijay1712@gmail.com>
2022-11-14 17:15:20 +0800

    Menu Program to display score results and stars based on score with Pseudocode

--- CP1404 Practicals/prac_02/score_menu.py
+++ CP1404 Practicals/prac_02/score_menu.py
@@ -1,4 +1,53 @@
 """Pseudocode
+
+function main():
+    score = 0
+    MENU = "(G)et a valid score (must be 0-100 inclusive)\n(P)rint result\n(S)how stars\n(Q)uit: "
+    get choice
+    while choice != 'Q':
+        if choice == 'G':
+            score = get_score()
+        elif choice == 'P':
+            if score == 0:
+                score = get_score()
+            result = get_result(score)
+        display result
+        elif choice == 'S':
+            if score == 0:
+                score = get_score()
+            display_stars(score)
+        else:
+            display "Invalid choice"
+        choice = input(MENU)
+    display "Farewell"
+
+
+function get_score():
+    get score
+
+    while score < 0 or score > 100:
+        print("Invalid score")
+        get score
+    return score
+
+
+function get_result(score):
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+function display_stars(score):
+    for i in range(int(score)):
+        display '*', end=''
+
+
+
+main()
+
 """
 
 
