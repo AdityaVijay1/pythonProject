6851526a9af945a08797b0e7211540210845f9d0
AdityaVijay1 <adityavijay1712@gmail.com>
2022-12-05 17:37:42 +0800

    Practical Week 5

--- CP1404 Practicals/prac_05/state_names.py
+++ CP1404 Practicals/prac_05/state_names.py
@@ -20,10 +20,13 @@ for i in CODE_TO_NAME:
 print()
 
 state_code = input("Enter short state: ").upper()
-
+state_codes=[]
 while state_code != '':
     try:
         print(state_code, "is", CODE_TO_NAME[state_code])
+        state_codes.append(state_code)
     except KeyError:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
+for i in state_codes:
+    print(f"{i} is {CODE_TO_NAME[i]}")
