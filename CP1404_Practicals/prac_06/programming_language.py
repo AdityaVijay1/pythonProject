157b814434ba17123b3f96983d872847d28f2fb5
AdityaVijay1 <adityavijay1712@gmail.com>
2022-12-12 17:40:52 +0800

    Added docstrings to explain each method

--- CP1404_Practicals/prac_06/programming_language.py
+++ CP1404_Practicals/prac_06/programming_language.py
@@ -6,6 +6,13 @@ Actual Time: 15 minutes"""
 
 class ProgrammingLanguage:
     def __init__(self, name='', typing='', reflection= bool, year=0):
+        """Initialise a Guitar instance.
+
+                        name: Name of the programming language
+                        typing: Type of programming language
+                        reflection: Reflection of the programming language
+                        year: Year of the programming language released
+                        """
         self.name = name
         self.typing = typing
         self.reflection = reflection
@@ -15,6 +22,7 @@ class ProgrammingLanguage:
         return f"{self.name},{self.typing} Typing, Reflection= {self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        '''Method to find out which programming language is dynamic'''
         if self.typing == 'Dynamic':
             return True
         else:
